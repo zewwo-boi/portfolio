b14e1812b9bee9b2020d7818d73b43b7f4570875
Ultimate Boi <nathanielkang08@gmail.com>
2022-06-23 22:26:46 +0900

    Start of portfolio :fire:
    Basic dependencies and structure

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    quoteProps: "consistent",
+    printWidth: 100,
+    trailingComma: "es5",
+    bracketSameLine: true,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    vueIndentScriptAndStyle: true,
+    plugins: [require("prettier-plugin-tailwindcss")],
+};
