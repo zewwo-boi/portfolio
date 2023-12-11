4e4572c0ae1a1ef5f6ba85b33df9a5da1b4bbc71
Nathaniel <59523827+zewwo-boi@users.noreply.github.com>
2023-12-11 21:29:29 +0900

    🚚 eslint config with type

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/** @type { import("eslint").Linter.FlatConfig[] } */
+export default [
+    {
+        extends: ["next/core-web-vitals", "plugin:@react-three/recommended"],
+        rules: {
+            "react/no-unknown-property": ["error", { ignore: ["css", "jsx"] }],
+        },
+    },
+];
