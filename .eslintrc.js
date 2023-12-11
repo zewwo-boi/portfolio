fdca774f2c7aeeeecc7f018fb48731100578d650
Nathaniel <59523827+zewwo-boi@users.noreply.github.com>
2023-12-11 21:30:05 +0900

    🔨 fix annoying hook warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ export default [
         extends: ["next/core-web-vitals", "plugin:@react-three/recommended"],
         rules: {
             "react/no-unknown-property": ["error", { ignore: ["css", "jsx"] }],
+            "react-hooks/exhaustive-deps": [0],
         },
     },
 ];
