b078a146a3583754b27de7aa9c165ec54d274bcb
Nathaniel <59523827+zewwo-boi@users.noreply.github.com>
2023-12-12 19:31:47 +0900

    ♻️ change props and types

--- src/components/Party/Blob/Blob.tsx
+++ src/components/Party/Blob/Blob.tsx
@@ -1,22 +1,23 @@
 /* eslint-disable react/no-unknown-property */
 import { MeshProps, useFrame } from "@react-three/fiber";
+import { MotionProps } from "framer-motion";
+import { motion } from "framer-motion-3d";
 import { useMemo, useRef } from "react";
 import { Mesh } from "three";
 
 import vertexShader from "@/components/Party/Blob/vertexShader.vert";
 
-interface Props {
+interface Props extends Partial<MotionProps & MeshProps> {
     fragment_shader: string;
     intensity?: number;
-    args?: Partial<MeshProps>;
 }
 
-function Blob({ fragment_shader, intensity = 0.8, args }: Props) {
-    const mesh = useRef<Mesh>();
+function Blob({ intensity = 0.8, fragment_shader, ...props }: Props) {
+    const mesh = useRef<Mesh>(null!);
     const uniforms = useMemo(() => {
         return {
             u_time: { value: 0 },
-            u_intensity: { value: intensity },
+            u_intensity: { value: intensity ?? 0.8 },
             u_seed: { value: Math.random() * 1000 },
         };
     }, []);
@@ -25,20 +26,22 @@ function Blob({ fragment_shader, intensity = 0.8, args }: Props) {
         const { clock } = cb;
 
         if (mesh.current) {
+            // TODO: There has got to be a better way than this
             // @ts-expect-error
             mesh.current.material.uniforms.u_time.value = 0.04 * clock.getElapsedTime();
         }
     });
 
     return (
-        <mesh ref={mesh} scale={1.5} {...args}>
+        // @ts-expect-error
+        <motion.mesh ref={mesh} scale={1.5} {...props}>
             <icosahedronGeometry args={[2, 20]} />
             <shaderMaterial
                 vertexShader={vertexShader}
                 fragmentShader={fragment_shader}
                 uniforms={uniforms}
             />
-        </mesh>
+        </motion.mesh>
     );
 }
 
