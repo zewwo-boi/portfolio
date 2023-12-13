41b6f7872164f6f22a35295f1598bd02a9a8f200
Nathaniel <59523827+zewwo-boi@users.noreply.github.com>
2023-12-13 13:20:01 +0900

    🏷️ improve types

--- src/components/Party/Blob/Blob.tsx
+++ src/components/Party/Blob/Blob.tsx
@@ -1,19 +1,17 @@
 /* eslint-disable react/no-unknown-property */
 import { MeshProps, useFrame } from "@react-three/fiber";
-import { MotionProps } from "framer-motion";
 import { motion } from "framer-motion-3d";
 import { useMemo, useRef } from "react";
-import { Mesh } from "three";
 
 import vertexShader from "@/components/Party/Blob/vertexShader.vert";
 
-interface Props extends Partial<MotionProps & MeshProps> {
+interface Props extends Partial<typeof motion.mesh.defaultProps> {
     fragment_shader: string;
     intensity?: number;
 }
 
 function Blob({ intensity = 0.8, fragment_shader, ...props }: Props) {
-    const mesh = useRef<Mesh>(null!);
+    const mesh = useRef<MeshProps>(null);
     const uniforms = useMemo(() => {
         return {
             u_time: { value: 0 },
@@ -26,14 +24,13 @@ function Blob({ intensity = 0.8, fragment_shader, ...props }: Props) {
         const { clock } = cb;
 
         if (mesh.current) {
-            // TODO: There has got to be a better way than this
+            // TODO: Refine types
             // @ts-expect-error
             mesh.current.material.uniforms.u_time.value = 0.04 * clock.getElapsedTime();
         }
     });
 
     return (
-        // @ts-expect-error
         <motion.mesh ref={mesh} scale={1.5} {...props}>
             <icosahedronGeometry args={[2, 20]} />
             <shaderMaterial
--- src/components/Party/Wave/Wave.tsx
+++ src/components/Party/Wave/Wave.tsx
@@ -2,18 +2,16 @@
 import { MeshProps, useFrame } from "@react-three/fiber";
 import { motion } from "framer-motion-3d";
 import { useMemo, useRef } from "react";
-import { Mesh } from "three";
 
 import vertexShader from "@/components/Party/Wave/wave.vert";
-import { MotionProps } from "framer-motion";
 
-interface Props extends Partial<MotionProps & MeshProps> {
+interface Props extends Partial<typeof motion.mesh.defaultProps> {
     fragment_shader: string;
     intensity?: number;
 }
 
 function Wave({ fragment_shader, intensity = 0.2, ...props }: Props) {
-    const mesh = useRef<Mesh>();
+    const mesh = useRef<MeshProps>(null);
     const uniforms = useMemo(() => {
         return {
             u_time: { value: 0 },
@@ -31,7 +29,6 @@ function Wave({ fragment_shader, intensity = 0.2, ...props }: Props) {
     });
 
     return (
-        // @ts-expect-error
         <motion.mesh ref={mesh} scale={1} {...props} rotation={[Math.PI / -2, 0, 0]}>
             <planeGeometry args={[16, 4, 160, 40]} />
             <shaderMaterial
