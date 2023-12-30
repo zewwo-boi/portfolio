8450d0dc7dbf488ab1b14f07e6df066a29f67beb
Nathaniel <59523827+zewwo-boi@users.noreply.github.com>
2023-12-30 21:17:39 +0900

    ✨ change to pencil

--- src/pages/about.parallax.tsx
+++ src/pages/about.parallax.tsx
@@ -38,40 +38,78 @@ interface IObserve extends IScroll {
 
 type GLTFResult = GLTF & {
     nodes: {
-        Xbox_Controller: THREE.Mesh;
+        Circle004_Circle008_1: THREE.Mesh;
+        Circle004_Circle008_1_1: THREE.Mesh;
+        Circle004_Circle008_1_2: THREE.Mesh;
+        Circle004_Circle008_1_3: THREE.Mesh;
+        Circle004_Circle008_1_4: THREE.Mesh;
     };
     materials: {
-        Mat: THREE.MeshStandardMaterial;
+        FF9800: THREE.MeshStandardMaterial;
+        F06292: THREE.MeshStandardMaterial;
+        ["455A64"]: THREE.MeshStandardMaterial;
+        DD9944: THREE.MeshStandardMaterial;
+        ["1A1A1A"]: THREE.MeshStandardMaterial;
     };
 };
-
-interface IGameControllerProps extends Partial<typeof motion3d.group.defaultProps> {
+interface IPencilProps extends Partial<typeof motion3d.group.defaultProps> {
     opacity?: number;
 }
 
-export function GameController({ opacity = 1, ...props }: IGameControllerProps) {
-    let { nodes, materials } = useGLTF("/models/Game Controller.glb") as GLTFResult;
+export function Pencil({ opacity = 1, ...props }: IPencilProps) {
+    let { nodes, materials } = useGLTF("/models/Pencil.glb") as GLTFResult;
 
     return (
         <motion3d.group {...props} dispose={null}>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.Xbox_Controller.geometry}
-                material={materials.Mat}
+                geometry={nodes.Circle004_Circle008_1.geometry}
+                material={materials.FF9800}
                 material-transparent
                 material-opacity={opacity}
-                userData={{ name: "Xbox_Controller" }}
+            />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Circle004_Circle008_1_1.geometry}
+                material={materials.F06292}
+                material-transparent
+                material-opacity={opacity}
+            />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Circle004_Circle008_1_2.geometry}
+                material={materials["455A64"]}
+                material-transparent
+                material-opacity={opacity}
+            />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Circle004_Circle008_1_3.geometry}
+                material={materials.DD9944}
+                material-transparent
+                material-opacity={opacity}
+            />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Circle004_Circle008_1_4.geometry}
+                material={materials["1A1A1A"]}
+                material-transparent
+                material-opacity={opacity}
             />
         </motion3d.group>
     );
 }
 
 function Objects({ ratioY, scrollY, ...observes }: IObserve) {
-    const GameControllerUnit = useTransform(scrollY, [3.5, 4.9], [0, 1]);
-    const [GameControllerOpacity, setGameControllerOpacity] = useState(0);
-
+    const PencilUnit = useTransform(scrollY, [3.5, 4.9], [0, 1]);
+    const [PencilOpacity, setPencilOpacity] = useState(0);
     const rendered = useRef(false);
+
     useEffect(() => {
         if (!rendered.current) {
             rendered.current = true;
@@ -80,15 +118,17 @@ function Objects({ ratioY, scrollY, ...observes }: IObserve) {
 
         if (observes.third) {
             animate(0, 1, {
-                onUpdate: (val) => setGameControllerOpacity(val),
+                onUpdate: (val) => setPencilOpacity(val),
                 duration: 1,
             });
         } else {
             animate(1, 0, {
-                onUpdate: (val) => setGameControllerOpacity(val),
+                onUpdate: (val) => setPencilOpacity(val),
                 duration: 1,
             });
         }
+
+        console.log(observes.third);
     }, [observes.third]);
 
     return (
@@ -108,13 +148,13 @@ function Objects({ ratioY, scrollY, ...observes }: IObserve) {
                 animate={observes.second ? { y: -2 } : {}}
                 transition={{ type: "spring", stiffness: 45 }}
             />
-            <GameController
+            <Pencil
                 name="3"
-                position={new Vector3(0, 0, -8 * GameControllerUnit.get())}
+                position={new Vector3(0, 0, -8 * PencilUnit.get())}
                 rotation-x={Math.PI / 2}
-                rotation-y={GameControllerUnit.get() * 2 * Math.PI}
+                rotation-y={PencilUnit.get() * 2 * Math.PI}
                 scale={0.2}
-                opacity={GameControllerOpacity}
+                opacity={PencilOpacity}
             />
         </>
     );
@@ -124,7 +164,7 @@ function Elements({ ratioY, scrollY }: IScroll) {
     const [first, setFirst] = useState(false);
     const [second, setSecond] = useState(false);
     const [third, setThird] = useState(false);
-    const [fourth, setFourth] = useState(false);
+    const [third1, setThird1] = useState(false);
 
     useEffect(() => {
         if (ratioY <= 0.6) setFirst(true);
@@ -136,8 +176,8 @@ function Elements({ ratioY, scrollY }: IScroll) {
         if (3.6 <= ratioY && ratioY <= 5) setThird(true);
         else setThird(false);
 
-        if (5.6 <= ratioY) setFourth(true);
-        else setFourth(false);
+        if (5.6 <= ratioY) setThird1(true);
+        else setThird1(false);
     }, [ratioY]);
 
     return (
@@ -160,18 +200,18 @@ function Elements({ ratioY, scrollY }: IScroll) {
                     initial={{ x: "20vw" }}
                     animate={first ? {} : { x: "-30vw" }}
                     transition={{ type: "spring", stiffness: 65, restDelta: 0.005 }}>
-                    I go by lots of names
+                    Hello
                 </motion.div>
             </section>
             <section className="names h-[240vh]">
                 <motion.div
                     className={classNames(
                         `fixed left-1/2 top-1/2 h-12 -translate-x-1/2 text-center text-5xl font-bold`,
-                        (1 >= ratioY || ratioY >= 3) && "hidden"
+                        (0.8 >= ratioY || ratioY >= 3) && "hidden"
                     )}
                     initial={{ opacity: 0 }}
                     animate={second ? { opacity: 1 } : {}}>
-                    Sometimes they refer to me as{" "}
+                    I am{" "}
                     <motion.span
                         initial={{ opacity: 0 }}
                         animate={second ? { opacity: 1, transition: { delay: 1 } } : {}}>
@@ -179,47 +219,22 @@ function Elements({ ratioY, scrollY }: IScroll) {
                     </motion.span>
                 </motion.div>
             </section>
-            <section className="h-[200vh]">
+            <section className="h-[400vh]">
                 <motion.div
                     className={classNames(
                         `fixed bottom-[30vh] left-1/2 -translate-x-1/2 text-center text-4xl font-bold`,
-                        (3.2 >= ratioY || ratioY >= 5.4) && "hidden"
+                        (3.2 >= ratioY || ratioY >= 7) && "hidden"
                     )}
                     initial={{ opacity: 0 }}
                     animate={third ? { opacity: 1 } : {}}>
-                    <motion.div>In games, they call me... </motion.div>
+                    <motion.div>I am a</motion.div>
                     <motion.div
                         initial={{ opacity: 0 }}
                         animate={third ? { opacity: 1 } : {}}
                         transition={{ delay: 1 }}>
-                        ak1004nk
+                        student
                     </motion.div>
                 </motion.div>
-            </section>
-            <section className="h-[260vh]">
-                <motion.div
-                    className={classNames(
-                        `fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-5xl font-bold`,
-                        !fourth && "hidden"
-                    )}
-                    initial={{ opacity: 0 }}
-                    animate={fourth ? { opacity: 1 } : {}}>
-                    <motion.div animate={fourth ? { opacity: 0 } : {}} transition={{ delay: 1 }}>
-                        But in real life...
-                    </motion.div>
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={fourth ? { opacity: 1 } : {}}
-                        transition={{ delay: 1.5 }}>
-                        I am (insert incredible virtue)
-                    </motion.div>
-                </motion.div>
-            </section>
-            <section className="credits">
-                Videogame Controller by Poly by Google [CC-BY] via{" "}
-                <a href="https://poly.pizza/m/6365MG_Pr_f" target="_blank">
-                    Poly Pizza
-                </a>
             </section>
         </>
     );
@@ -243,3 +258,5 @@ export default function Parallax() {
         </>
     );
 }
+
+useGLTF.preload("/models/Pencil.glb");
